309426b7fda0c5ac3a4a46b54caf68fb10033861
Junaid Iqbal <junaidiqbal0323@gmail.com>
2021-07-31 23:11:41 +0500

    more detail in notification and counter bug fix

--- foo/views.py
+++ foo/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 from django.views.generic import ListView
+from notifications.models import Notification
+
 from .models import Badassness
 
 
@@ -9,4 +11,12 @@ class BadassListView(ListView):
     model = Badassness
     paginate_by = 5
     context_object_name = 'badasses'
+
     template_name = 'foo/badass_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(BadassListView, self).get_context_data(**kwargs)
+        notifications = Notification.objects.all()
+        context['notification'] = notifications
+        context['count'] = Notification.objects.filter(unread=True).count()
+        return context
